3e89199590cd3e2765b13f11c7c6d0740ac153be
Aneesh Naik <aneesh92@hotmail.co.uk>
2023-05-03 11:22:53 +0100

    New fig4 script

--- figures/figX_median_vlos_map.py
+++ figures/figX_median_vlos_map.py
@@ -18,14 +18,20 @@ sys.path.append("..")
 from src.utils import get_datadir
 
 
-def calc_vlos_map(X, Y, V, N_bins, X_min, X_max, Y_min, Y_max):
+def calc_vlos_map(X, Y, V, SIGD, SIGV, N_bins, X_min, X_max, Y_min, Y_max,
+                  SIGD_cut, SIGV_cut):
 
     # bin edges
     X_edges = np.linspace(X_min, X_max, N_bins + 1)
     Y_edges = np.linspace(Y_min, Y_max, N_bins + 1)
 
-    # cut to region
-    m = (X > X_min) & (X < X_max) & (Y > Y_min) & (Y < Y_max)
+    # cut to region and quality
+    m = ((X > X_min)
+         & (X < X_max)
+         & (Y > Y_min)
+         & (Y < Y_max)
+         & (SIGV < SIGV_cut)
+         & (SIGD < SIGD_cut))
     X = X[m]
     Y = Y[m]
     V = V[m]
@@ -66,7 +72,9 @@ def calc_vlos_map(X, Y, V, N_bins, X_min, X_max, Y_min, Y_max):
     return mu
 
 
-def create_plot_data(dfile, N_bins, X_min, X_max, Y_min, Y_max):
+def create_plot_data(
+        dfile, N_bins, X_min, X_max, Y_min, Y_max, SIGD_cut, SIGV_cut
+):
 
     # load data
     print("Loading data:")
@@ -75,17 +83,26 @@ def create_plot_data(dfile, N_bins, X_min, X_max, Y_min, Y_max):
     X5 = data['X_5D']
     Y5 = data['Y_5D']
     V5 = data['V_5D']
+    SIGD5 = data['SIGD_5D']
+    SIGV5 = data['SIGV_5D']
     X6 = data['X_6D']
     Y6 = data['Y_6D']
     V6 = data['V_6D']
+    SIGD6 = data['SIGD_6D']
+    SIGV6 = data['SIGV_6D']
     print(">>>Done.\n")
 
     # construct maps
     print("5D map:")
-    mu5 = calc_vlos_map(X5, Y5, V5, N_bins, X_min, X_max, Y_min, Y_max)
+    mu5 = calc_vlos_map(
+        X5, Y5, V5, SIGD5, SIGV5,
+        N_bins, X_min, X_max, Y_min, Y_max, SIGD_cut, SIGV_cut
+    )
     print(">>>Done.\n")
     print("6D map:")
-    mu6 = calc_vlos_map(X6, Y6, V6, N_bins, X_min, X_max, Y_min, Y_max)
+    mu6 = calc_vlos_map(
+        X6, Y6, V6, SIGD6, SIGV6,
+        N_bins, X_min, X_max, Y_min, Y_max, SIGD_cut, SIGV_cut)
     print(">>>Done.\n")
 
     np.savez(dfile, mu5=mu5, mu6=mu6)
@@ -100,12 +117,16 @@ if __name__ == "__main__":
     X_max = 1
     Y_min = -9
     Y_max = 9
+    SIGD_cut = 1.5
+    SIGV_cut = 80
 
     # load plot data (create if not present)
     ddir = get_datadir()
     dfile = ddir + "figures/figX_median_vlos_map_data.npz"
     if not exists(dfile):
-        create_plot_data(dfile, N_bins, X_min, X_max, Y_min, Y_max)
+        create_plot_data(
+            dfile, N_bins, X_min, X_max, Y_min, Y_max, SIGD_cut, SIGV_cut
+        )
     data = np.load(dfile)
     mu5 = data['mu5']
     mu6 = data['mu6']
@@ -121,8 +142,9 @@ if __name__ == "__main__":
     Y_cens = 0.5 * (Y_edges[1:] + Y_edges[:-1])
 
     # plot settings
+    extent = [X_max, X_min, Y_max, Y_min]
     imargs = dict(
-        origin='lower', vmax=110, vmin=-110, extent=[X_max, X_min, Y_max, Y_min],
+        origin='lower', vmax=110, vmin=-110, extent=extent,
         cmap='Spectral_r', interpolation='none'
     )
     gridargs = dict(c='k', ls='dotted', lw=0.5)
@@ -158,15 +180,15 @@ if __name__ == "__main__":
     ax1.scatter([-8.122], [0], c='k', marker='x', s=20)
 
     # aximuthal grid lines
-    for R in np.arange(2, 16, 2):
-        phi = np.linspace(np.pi / 2, 3 * np.pi / 2, 500)
+    for R in np.arange(2, 20, 2):
+        phi = np.linspace(0, 2 * np.pi, 500)
         X = R * np.cos(phi)
         Y = R * np.sin(phi)
         ax0.plot(X, Y, **gridargs)
         ax1.plot(X, Y, **gridargs)
 
     # radial grid lines
-    for phi in np.linspace(np.pi / 2, 3 * np.pi / 2, 9):
+    for phi in np.linspace(0, 2 * np.pi, 17):
         R = np.linspace(0, 20, 500)
         X = R * np.cos(phi)
         Y = R * np.sin(phi)
