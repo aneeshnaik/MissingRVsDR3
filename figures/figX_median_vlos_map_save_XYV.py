aebd90a5ed5a1c5670324d785529575d4c3c5ce8
Aneesh Naik <aneesh92@hotmail.co.uk>
2023-05-03 08:54:28 +0100

    Change intermediate script to include sig_distance, sig_velocity

--- figures/figX_median_vlos_map_save_XYV.py
+++ figures/figX_median_vlos_map_save_XYV.py
@@ -26,8 +26,13 @@ def get_XYV_6D():
     cols = ['radial_velocity', 'ra', 'dec']
     df = pd.read_csv(ddir + 'DR3_6D/DR3_6D.csv', usecols=cols)
 
-    # read distance samples
-    d = np.load(ddir + 'DR3_6D/distance_samples.npy')[:, 0]
+    # read distance samples ORIGINAL
+    #d = np.load(ddir + 'DR3_6D/distance_samples.npy')[:, 0]
+
+    # read distance samples NEW
+    samples = np.load(ddir + 'DR3_6D/distance_samples.npy')
+    d = np.median(samples, axis=-1)
+    SIGD = np.std(samples, axis=-1)
 
     # convert coordinates
     X, Y, Z = convert_pos(df['ra'], df['dec'], d).T
@@ -36,8 +41,10 @@ def get_XYV_6D():
     X = X.astype(np.float32)
     Y = Y.astype(np.float32)
     V = df['radial_velocity'].to_numpy().astype(np.float32)
+    SIGD = SIGD.astype(np.float32)
+    SIGV = df['radial_velocity_error'].to_numpy.astype(np.float32)
 
-    return X, Y, V
+    return X, Y, V, SIGD, SIGV
 
 
 def get_XYV_5D(i):
@@ -47,8 +54,13 @@ def get_XYV_5D(i):
     df = pd.read_hdf(ddir + f"DR3_5D/{i}.hdf5")
     N = len(df)
 
+    # distance matrix NEW
+    d_matrix = np.array([df[f'd{j}'] for j in range(10)]).T
+    d = d_matrix[:, 0]
+    SIGD = np.std(d_matrix, axis=-1)
+
     # convert coordinates
-    X, Y, Z = convert_pos(df['ra'], df['dec'], df['d0']).T
+    X, Y, Z = convert_pos(df['ra'], df['dec'], d).T
 
     # load predictions
     preds = np.load(ddir + f"DR3_predictions/5D_{i}.npy")
@@ -58,38 +70,47 @@ def get_XYV_5D(i):
     indices = np.random.randint(0, preds.shape[1], size=N)
     V = preds[np.arange(N), indices]
 
+    # v uncertainty NEW
+    SIGV = 0.5 * np.diff(np.percentile(preds, (16, 84), axis=-1), axis=0)[0]
+
     # reduce precision
     X = X.astype(np.float32)
     Y = Y.astype(np.float32)
     V = V.astype(np.float32)
+    SIGD = SIGD.astype(np.float32)
+    SIGV = SIGV.astype(np.float32)
 
-    return X, Y, V
+    return X, Y, V, SIGD, SIGV
 
 
 if __name__ == "__main__":
 
     # get 6D data
     print("Getting 6D data:", flush=True)
-    X_6D, Y_6D, V_6D = get_XYV_6D()
+    X_6D, Y_6D, V_6D, SIGD_6D, SIGV_6D = get_XYV_6D()
     print(">>>Done.\n", flush=True)
 
     # loop over 5D datasets
     X_5D = np.array([], dtype=np.float32)
     Y_5D = np.array([], dtype=np.float32)
     V_5D = np.array([], dtype=np.float32)
+    SIGD_5D = np.array([], dtype=np.float32)
+    SIGV_5D = np.array([], dtype=np.float32)
     print("Getting 5D data:", flush=True)
     for i in trange(32):
         if not exists(get_datadir() + f"DR3_5D/{i}.hdf5"):
             continue
-        X, Y, V = get_XYV_5D(i)
+        X, Y, V, SIGD, SIGV = get_XYV_5D(i)
         X_5D = np.append(X_5D, X)
         Y_5D = np.append(Y_5D, Y)
         V_5D = np.append(V_5D, V)
+        SIGD_5D = np.append(SIGD_5D, SIGD)
+        SIGV_5D = np.append(SIGV_5D, SIGV)
     print(">>>Done.\n", flush=True)
 
     # save
     print("Saving:", flush=True)
     np.savez(get_datadir() + 'figures/figX_median_vlos_map_XYV_data.npz',
-             X_6D=X_6D, Y_6D=Y_6D, V_6D=V_6D,
-             X_5D=X_5D, Y_5D=Y_5D, V_5D=V_5D)
+             X_6D=X_6D, Y_6D=Y_6D, V_6D=V_6D, SIGD_6D=SIGD_6D, SIGV_6D=SIGV_6D,
+             X_5D=X_5D, Y_5D=Y_5D, V_5D=V_5D, SIGD_5D=SIGD_5D, SIGV_5D=SIGV_5D)
     print(">>>Done.\n", flush=True)
