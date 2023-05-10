f954967300d74875f552619617a613bad5ee585c
not populated <anaik@cirrus-login2.ib0.icexa.epcc.ed.ac.uk>
2023-05-10 21:26:08 +0100

    Subscripts for GMM scripts

--- predictions/submit_fit_GMM_6D.sh
+++ predictions/submit_fit_GMM_6D.sh
@@ -5,9 +5,9 @@
 #SBATCH --partition=gpu
 #SBATCH --qos=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --time=2-00:00:00
+#SBATCH --time=06:00:00
 #SBATCH --account=sc094
-#SBATCH --job-name=BNN
+#SBATCH --job-name=GMM6D
 
 # change to submission directory
 cd $SLURM_SUBMIT_DIR
@@ -22,4 +22,4 @@ export MRVDR3DDIR=/work/sc094/sc094/anaik/Data/MissingRVsDR3Data/
 eval "$(/work/sc094/sc094/anaik/miniconda3/bin/conda shell.bash hook)"
 source activate MissingRVs
 
-python compile_GMM_catalogue_6D_test.py 
+python fit_GMM_batch_6D_test.py $SLURM_ARRAY_TASK_ID 
